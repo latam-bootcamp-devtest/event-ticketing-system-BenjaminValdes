6e5031f7587cc50316f02190fc2f662f50499c3c
Benjamin Valdes <benjamin_valdes@hotmail.com>
2025-03-08 12:02:34 -0300

    feat(getEvents): added function to get all events

--- backend/routes.ts
+++ backend/routes.ts
@@ -12,6 +12,31 @@ const pool = new Pool({
   port: Number(process.env.DB_PORT)
 })
 
+const getAllEvents = async (request: Request, response: Response): Promise<void> => {
+  const page = parseInt(request.params.page) || 1
+  const pageSize = parseFloat(request.params.pagesize)
+
+  try {
+    const result =  await pool.query('SELECT * FROM event WHERE date > NOW() ORDER BY date ASC')
+    const allEvents = result.rows
+    const totalEvents = allEvents.length
+
+    const startIndex = (page - 1) * pageSize
+    const endIndex = startIndex + pageSize - 1
+    const paginatedEvents = allEvents.slice(startIndex, endIndex)
+
+    response.status(200).json({
+      currentPage: page,
+      pageSize,
+      totalPages: Math.ceil(result.rows.length / pageSize),
+      events: paginatedEvents,
+    })
+  } catch (error) {
+    console.error(error)
+    response.status(500).json({ error: 'Error fetching events' })
+  }
+}
+
 const createEvent = async (request: Request, response: Response): Promise<void> => {
   const { name, date, availableSeats } = request.body
 
@@ -40,5 +65,6 @@ const createEvent = async (request: Request, response: Response): Promise<void> 
 }
 
 router.post('/events', createEvent)
+router.get('/events/:page/:pagesize', getAllEvents)
 
 export default router
